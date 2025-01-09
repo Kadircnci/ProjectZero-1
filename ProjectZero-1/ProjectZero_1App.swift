1ae16791ed30406552c00737bbe1c14f4c335c5f
Kadircnci <kadircncii@gmail.com>
2025-01-09 14:26:45 +0300

    Initial Commit

---
+++ ProjectZero-1/ProjectZero_1App.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectZero_1App.swift
+//  ProjectZero-1
+//
+//  Created by Kadir Çanakcı on 9.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ProjectZero_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
