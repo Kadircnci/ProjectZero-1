f5a0c9cb9fcf0887d34e6172ab628431fb83d8e4
Kadircnci <kadircncii@gmail.com>
2025-01-09 14:39:10 +0300

    Added Buttons.Swift

---
+++ ProjectZero-1/Buttons.swift
@@ -0,0 +1,18 @@
+//
+//  Buttons.swift
+//  ProjectZero-1
+//
+//  Created by Kadir Çanakcı on 9.01.2025.
+//
+
+import SwiftUI
+
+struct Buttons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Buttons()
+}
