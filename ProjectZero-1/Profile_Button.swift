98a325b365172627a3122ee139b9852b8528fa80
Kadircnci <kadircncii@gmail.com>
2025-01-09 14:50:31 +0300

    BagButton Added

---
+++ ProjectZero-1/Profile_Button.swift
@@ -0,0 +1,19 @@
+//
+//  Profile_Button.swift
+//  ProjectZero-1
+//
+//  Created by Kadir Çanakcı on 9.01.2025.
+//
+
+import SwiftUI
+
+struct Profile_Button: View {
+    var body: some View {
+        Image(systemName: "bag")
+            .resizable()
+    }
+}
+
+#Preview {
+    Profile_Button()
+}
